858bad71cd3d20946647401cc7c936d5af0cc97b
Chris Fernando <c.fernando@optusnet.com.au>
2021-07-25 13:01:26 +0000

    combined idle power off and scheduled power on

---
+++ ec2_power_management/ec2_power_management/trigger.py
@@ -0,0 +1,258 @@
+import boto3
+import logging, sys
+import json
+import datetime
+from pytz import timezone
+
+
+def isIdleExempt(instance):
+    try:
+        for t in instance["Tags"]:
+            if str(t["Key"]).upper() == "IDLE_EXEMPT":
+                # found the tag we want
+                if str(t["Value"]).upper() == "TRUE":
+                    # true - exempt from idle shutdown
+                    logging.warning(
+                        f"[Power off] {instance['InstanceId']}: Instance is exempt from idle shutdown"
+                    )
+                    return True
+
+                elif str(t["Value"]).upper() == "FALSE":
+                    logging.warning(
+                        f"[Power off] {instance['InstanceId']}: Instance is explicitly NOT exempt from idle shutdown"
+                    )
+                    return False
+
+                else:
+                    logging.warning(
+                        f"[Power off] {instance['InstanceId']}: Found unrecognised tag value: {t['Value']}.  Defaulting to NOT exempt"
+                    )
+                    return False
+
+        # there were tags, but not the one we're looking for
+        logging.warning(
+            f"[Power off] {instance['InstanceId']}: Did not find tag IDLE_EXEMPT.  Defaulting to NOT exempt"
+        )
+        return False
+
+    except Exception as e:
+        # no tags, so default to not idle exempt
+        logging.warning(
+            f"[Power off] {instance['InstanceId']}: No tags on this instance.  Defaulting to NOT exempt",
+        )
+        return False
+
+
+def isScheduledPowerOn(instance):
+    try:
+        powerOnConditionMet = False
+        dayConditionMet = False
+        hourConditionMet = False
+        for t in instance["Tags"]:
+            if (
+                str(t["Key"]).upper() == "EC2_POWERON"
+                and str(t["Value"]).upper() == "TRUE"
+            ):
+                powerOnConditionMet = True
+                logging.warning(
+                    f"[Power on] {instance['InstanceId']}: Met EC2_POWERON tag condition"
+                )
+
+            if str(t["Key"]).upper() == "EC2_POWERON_SCHEDULE":
+                try:
+                    schedule_json = json.loads(t["Value"])
+                    if (
+                        "hour" not in schedule_json.keys()
+                        or "day" not in schedule_json.keys()
+                    ):
+                        logging.error(
+                            f"[Power on] {instance['InstanceId']}: EC2_POWERON_SCHEDULE json must include 'hour' and 'day' keys.  json: {t['Value']}"
+                        )
+                        return False
+
+                    try:
+                        int(schedule_json["hour"])
+                    except Exception as e:
+                        logging.error(
+                            f"[Power on] {instance['InstanceId']}: EC2_POWERON_SCHEDULE 'hour' value must be an integer.  json: {t['Value']}"
+                        )
+                        raise
+
+                    if int(schedule_json["hour"]) > 23:
+                        logging.error(
+                            f"[Power on] {instance['InstanceId']}: EC2_POWERON_SCHEDULE 'hour' value must be <24.  json: {t['Value']}"
+                        )
+                        return False
+
+                    if (
+                        str(schedule_json["day"]).upper()
+                        == str(datetime.datetime.today().strftime("%A")).upper()
+                    ):
+                        dayConditionMet = True
+
+                    now = datetime.datetime.now(timezone("Australia/Melbourne"))
+                    if now.hour == int(schedule_json["hour"]):
+                        hourConditionMet = True
+
+                    logging.error(
+                        f"[Power on] {instance['InstanceId']}: EC2_POWERON_SCHEDULE contains valid json schedule: {t['Value']}"
+                    )
+
+                except Exception as e:
+                    # bad json
+                    logging.error(
+                        f"[Power on] {instance['InstanceId']}: Found EC2_POWERON_SCHEDULE tag, but could not parse content as JSON - ignoring.  Found: {t['Value']}"
+                    )
+                    return False
+
+        # finished looking for tags, see if we found them
+        if powerOnConditionMet and dayConditionMet and hourConditionMet:
+            # matched so do it
+            logging.warning(
+                f"[Power on] {instance['InstanceId']}: Satisfied EC2_POWERON and EC2_POWERON_SCHEDULE conditions"
+            )
+            return True
+
+        else:
+            if not powerOnConditionMet:
+                logging.warning(
+                    f"[Power on] {instance['InstanceId']}: Did not satisfy EC2_POWERON condition"
+                )
+            if not dayConditionMet:
+                logging.warning(
+                    f"[Power on] {instance['InstanceId']}: Did not satisfy EC2_POWERON_SCHEDULE day condition"
+                )
+            if not hourConditionMet:
+                logging.warning(
+                    f"[Power on] {instance['InstanceId']}: Did not satisfy EC2_POWERON_SCHEDULE hour condition"
+                )
+            return False
+
+    except Exception as e:
+        logging.error(
+            f"[Power on] {instance['InstanceId']}: No tags on this instance.  Defaulting to NO schedule"
+        )
+        logging.error(str(e))
+        return False
+
+
+def trigger_handler(event, context):
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    logging.warning("Beginning trigger_handler")
+
+    ec2Client = boto3.client("ec2")
+    # i don't like this since its case sensitive for both keys and values
+    # instDict=client.describe_instances(
+    #        Filters=[{'Name':'tag:Environment','Values':['Dev']}]
+    #    )
+
+    logging.warning("[Power off] Beginning power off")
+
+    matchedPowerOffInstances = []
+    instDict = ec2Client.describe_instances(
+        Filters=[{"Name": "instance-state-name", "Values": ["running"]}]
+    )
+    logging.warning("[Power off] Ran describe instances")
+    try:
+        logging.warning(f"[Power off] Found {len(instDict)} reservations")
+        for r in instDict["Reservations"]:
+            logging.warning(f"[Power off] Found {len(r)} instances")
+            for i in r["Instances"]:
+                if isIdleExempt(instance=i):
+                    logging.warning(
+                        f"[Power off] {i['InstanceId']}: Instance is idle exempt, skipping"
+                    )
+                else:
+                    logging.warning(
+                        f"[Power off] {i['InstanceId']}: Instance is NOT idle exempt"
+                    )
+                    matchedPowerOffInstances.append(i)
+    except Exception as e:
+        logging.error(
+            "[Power off] Did not find Reservations or Instances key in Dictionary.  Terminating."
+        )
+        logging.error(str(e))
+        raise
+
+    logging.warning(
+        "[Power off] Finished iterating ec2 instances, now beginning invoke"
+    )
+    # Invoke worker function for each IP address
+    lambdaClient = boto3.client("lambda")
+    for host in matchedPowerOffInstances:
+        jsonPayload = {}
+        jsonPayload["IP"] = host["PrivateIpAddress"]
+        jsonPayload["InstanceId"] = host["InstanceId"]
+
+        logging.warning(
+            f"[Power off] {host['PrivateIpAddress']}: Invoking worker_function"
+        )
+        payload = json.dumps(jsonPayload)
+        logging.warning(f"[Power off] {host['PrivateIpAddress']}: Payload set")
+        invokeResponse = lambdaClient.invoke(
+            FunctionName="arn:aws:lambda:us-west-2:036372598227:function:ec2-power-management-WorkerFunction-xKBC7O6FaPHC",
+            InvocationType="Event",
+            LogType="Tail",
+            Payload=payload,
+        )
+        logging.warning(
+            f"[Power off] {host['PrivateIpAddress']}: Finished.  Response: {invokeResponse}"
+        )
+    if len(matchedPowerOffInstances) == 0:
+        logging.warning("[Power off] No instances online.  Finished successfully.")
+    else:
+        logging.warning(
+            "[Power off] Successfully issued shutdown commands.  Finished power off"
+        )
+
+    # i could combine the power off and on logic but this feels cleaner, easier to maintain even if its a bit heavy handed
+    logging.warning("[Power on] Beginning power on")
+
+    matchedPowerOffInstances = []
+    instDict = ec2Client.describe_instances(
+        Filters=[{"Name": "instance-state-name", "Values": ["stopped"]}]
+    )
+
+    logging.warning("[Power on] Ran describe instances")
+    try:
+        logging.warning(f"[Power on] Found {len(instDict)} instances")
+        for r in instDict["Reservations"]:
+            logging.warning(
+                f"[Power on] Found {len(r)} reservations and {len(r['Instances'])} instance"
+            )
+            for i in r["Instances"]:
+                if isScheduledPowerOn(instance=i):
+                    try:
+                        powerOnInstances = [i["InstanceId"]]
+                        ec2Client.start_instances(InstanceIds=powerOnInstances)
+                        logging.warning(
+                            f"[Power on] {i['InstanceId']} Met dayCondition and hourCondition, successfully issued power on command"
+                        )
+                    except Exception as e:
+                        logging.error(
+                            f"[Power on] {i['InstanceId']} Failed to power on instance"
+                        )
+                        logging.error(str(e))
+                        raise
+
+                else:
+                    # don't do the thing
+                    logging.warning(
+                        f"[Power on] {i['InstanceId']} Not tagged for power on, skipping"
+                    )
+
+            logging.warning(f"[Power on] Finished iterating ec2 instances")
+
+    except Exception as e:
+        logging.error(
+            "[Power on] Did not find Reservations or Instances key in Dictionary.  Terminating."
+        )
+        logging.error(str(e))
+        raise
+
+    logging.warning(f"[Power on] Successfully processed startups")
+    return {"statusCode": 200, "body": json.dumps("Success")}
+
+
+if __name__ == "__main__":
+    trigger_handler("", "")
---
+++ ec2_power_management/ec2_power_management/worker.py
@@ -0,0 +1,130 @@
+import boto3
+import paramiko
+import logging, sys
+import json
+
+
+def worker_handler(event, context):
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+    s3Client = boto3.client("s3")
+    # Download private key file from secure S3 bucket
+    s3Client.download_file("fdotech", "ssh_keys/chris2.pem", "/tmp/keyname.pem")
+    # s3Client.download_file("fdotech", "ssh_keys/chris2.pem", "y:\\keyname.pem")
+    logging.warning(f"{event['IP']}: Got pem file from S3")
+
+    k = paramiko.RSAKey.from_private_key_file("/tmp/keyname.pem")
+    # k = paramiko.RSAKey.from_private_key_file("y:\\keyname.pem")
+    c = paramiko.SSHClient()
+
+    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    host = event["IP"]
+    c.connect(hostname=host, username="ubuntu", pkey=k)
+    logging.warning(f"{host}: Connected via SSH")
+
+    command = "uptime"
+    stdin, stdout, stderr = c.exec_command(command)
+    uptimeResult = stdout.read()
+    uptimeResult = uptimeResult.decode("utf-8")
+
+    logging.warning(f"{host}: Uptime output is: {uptimeResult}")
+
+    uptimeList = uptimeResult.split()
+
+    # 20:38:49 up 2 min,  0 users,  load average: 0.28, 0.25, 0.10 pass
+    # 19:44:57 up 20:40,  0 users,  load average: 0.04, 0.01, 0.00 pass
+    # 19:44:57 up 20:40,  1 user,  load average: 0.04, 0.01, 0.00 pass
+    # 09:45:24 up 46 days, 11:51,  1 user,  load average: 0.11, 0.04, 0.01
+
+    # if position 2 contains :, then less than 24 hours
+    # otherwise, more than 1 day
+    if uptimeList[3] == "min,":
+        # up for less than an hour
+        uptimeConditionMet = False
+        logging.warning(
+            f"{host}: Server has been online for {uptimeList[2]} minutes - has not met uptime condition"
+        )
+    elif ":" in uptimeList[2]:
+        # up for less than a day
+        colLocation = uptimeList[2].find(":")
+
+        # just get hours
+        strPositionTwo = str(uptimeList[2])
+        hoursUptime = strPositionTwo[:colLocation]
+
+        if int(hoursUptime) > 0:
+            # been up for more than an hour
+            uptimeConditionMet = True
+            logging.warning(
+                f"{host}: Server has been online for {hoursUptime} hours - met uptime condition"
+            )
+        else:
+            # hasn't een online long enough to be deemed idle
+            uptimeConditionMet = False
+            logging.warning(
+                f"{host}: Server has been online for {hoursUptime} hours - has not met uptime condition"
+            )
+    else:
+        try:
+            hoursUptime = int(uptimeList[2])
+            uptimeConditionMet = True
+            logging.warning(
+                f"{host}: Server has been online for {hoursUptime} hours - met uptime condition"
+            )
+        except:
+            logging.error(f"{host}: Unprocessable string found: {uptimeList[2]}")
+
+    # can't rely on index, so find location of 'users' or 'user' and then go back one position
+    # maybe the hackiest code I've ever written (probably not...)
+    try:
+        userIndex = uptimeList.index("users,") - 1
+    except:
+        userIndex = uptimeList.index("user,") - 1
+
+    if int(uptimeList[userIndex]) > 0:
+        # someone is logged in
+        logging.warning(
+            f"{host}: {uptimeList[userIndex]} user(s) are logged in - has not met users condition"
+        )
+
+        usersConditionMet = False
+
+    else:
+        # nobody is logged in
+        logging.warning(f"{host}: No user(s) are logged in - has met users condition")
+
+        usersConditionMet = True
+
+    if uptimeConditionMet and usersConditionMet:
+        logging.warning(f"{host}: Server is deemed idle")
+        idle = True
+    else:
+        logging.warning(f"{host}: Server is NOT deemed idle")
+        idle = False
+
+    if idle:
+        # shut down
+        try:
+            ec2Client = boto3.client("ec2")
+            instanceList = [event["InstanceId"]]
+            ec2Client.stop_instances(InstanceIds=instanceList)
+            logging.warning(
+                f"{host}: Successfully issued shutdown command to idle system"
+            )
+
+        except Exception as e:
+            logging.error(
+                f"{host}: Failed to issue shutdown command to instance {event['InstanceId']}.  Error: {str(e)}"
+            )
+            logging.error(str(e))
+            raise
+    else:
+        # not idle, ignore
+        logging.warning(f"{host}: Non-idle system.  Skipping")
+
+    return {"statusCode": 200, "body": json.dumps("Success")}
+
+
+if __name__ == "__main__":
+    worker_handler({"IP": "18.236.118.125", "InstanceId": "i-09f0a44d7455c1e65"}, "")
